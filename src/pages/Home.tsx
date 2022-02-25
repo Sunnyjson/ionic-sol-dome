58ee2a94ec5f1e0a38255f7e655abd26206cf655
Sunny Cheung <sunnyzhang@ideaculture.com>
2022-02-25 16:48:49 +0800

    this is an icoic and sol dome

---
+++ src/pages/Home.tsx
@@ -0,0 +1,55 @@
+import { IonContent, IonPage } from "@ionic/react";
+
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider, WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SlopeWalletAdapter,
+  SolflareWalletAdapter,
+  SolletExtensionWalletAdapter,
+  SolletWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+import "./Home.css";
+
+const Home = () => {
+  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+  const network = WalletAdapterNetwork.Devnet;
+
+  // You can also provide a custom RPC endpoint.
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SlopeWalletAdapter(),
+      new SolflareWalletAdapter({ network }),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+      new SolletExtensionWalletAdapter({ network }),
+    ],
+    [network]
+  );
+
+  return (
+    <IonPage id='home-page'>
+      <IonContent fullscreen>
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+              <WalletMultiButton />
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
